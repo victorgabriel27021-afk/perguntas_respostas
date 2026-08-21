b89e8d4984f2ee5bfa034194d13ae7e6bbe0b8a6
Victor Gabriel <victorgabriel27021@gmail.com>
2026-08-21 16:34:53 -0300

    Cria base da aplicação de perguntas e respostas

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,25 @@
+class QuestionsController < ApplicationController
+  def index
+    @questions = Question.all
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+
+    if @question.save
+      redirect_to questions_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:description)
+  end
+end
