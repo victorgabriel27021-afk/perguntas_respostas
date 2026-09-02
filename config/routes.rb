7851914acc78c78da63c7380a10df232bd292134
Victor Gabriel <victorgabriel27021@gmail.com>
2026-09-02 19:11:53 -0300

    organizando e resolvendo problema nas  rotas do admin

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,7 @@ Rails.application.routes.draw do
   get "dashboard", to: "dashboard#index"
 
   resources :users, only: [:index, :show, :edit, :update, :destroy]
-  patch "users/:id", to: "users#update", as: :update_user
-  delete "users/:id", to: "users#destroy", as: :delete_user
-  patch "users/:id/toggle_admin", to: "users#toggle_admin", as: :toggle_admin_user
+patch "users/:id/toggle_admin", to: "users#toggle_admin", as: :toggle_admin_user
 end
 
   devise_for :users
